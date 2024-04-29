e7d5796953f73d7eb85a3bc13008090e77c1891e
Kevin Coraizaca <117871461+phkandresc@users.noreply.github.com>
2024-04-29 17:00:46 -0500

    ConsoleMenu v1.0

--- Practica/src/ConsoleMenu.java
+++ Practica/src/ConsoleMenu.java
@@ -1,15 +1,42 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class ConsoleMenu {
-    public List<String> menuItems;
+class ConsoleMenu{
+    ArrayList<String> menuItems;
+    String menuTitle;
 
-    public ConsoleMenu(String[] menuItems) {
-        this.menuItems = List.of(menuItems);
+    public ConsoleMenu(String title, String [] items){
+        menuTitle = title;
+        menuItems = new ArrayList<>(List.of(items));
+        menuItems.add("Exit");
     }
 
-    void printMenu() {
-        for (int i = 0; i < menuItems.size(); i++) {
-            System.out.println((i + 1) + ". " + menuItems.get(i));
+    public void displayMenu(){
+        System.out.println("*************************");
+        System.out.println(menuTitle);
+        System.out.println("*************************");
+        for(String item: menuItems){
+            System.out.println(menuItems.indexOf(item)+ 1+". " + item);
+        }
+        System.out.println("Choose an option:");
+    }
+
+    public int getOption(){
+        try{
+            int option = Integer.parseInt(System.console().readLine());
+            if(option > 0 && option <= menuItems.size()){
+                return option;
+            }
+            else{
+                System.out.println("Invalid option. Please try again.");
+                return getOption();
+            }
+        }
+        catch(NumberFormatException e){
+            System.out.println("Invalid option. Please try again.");
+            return getOption();
         }
     }
+
+
 }
--- Practica/src/Main.java
+++ Practica/src/Main.java
@@ -1,12 +1,29 @@
 public class Main {
-    static final String[] menuItems = {
-        "Add an item",
-        "List all items",
-        "Delete an item",
-        "Exit"
-    };
     public static void main(String[] args) {
-        ConsoleMenu menu = new ConsoleMenu(menuItems);
-        menu.printMenu();
+        String [] items = {"Add task", "Add task(Manual)", "View tasks"};
+        ConsoleMenu menu = new ConsoleMenu("Main Menu", items);
+
+        int option = -1;
+        while(option != menu.menuItems.size()){
+            menu.displayMenu();
+            option = menu.getOption();
+            switch(option){
+                case 1:
+                    System.out.println("Add task");
+                    break;
+                case 2:
+                    System.out.println("Add task(Manual)");
+                    break;
+                case 3:
+                    System.out.println("View tasks");
+                    break;
+                case 4:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+
     }
 }
